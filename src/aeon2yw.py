c8f668b6686129f77b61dc75c379e9025b98c6a0
peter88213 <peter.88213@t-online.de>
2021-07-14 21:49:55 +0200

    Prepare v0.3.0 based on PyWriter v3.12.9
    Start with the csv converter from the paeon project.

---
+++ src/aeon2yw.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Aeon Timeline 2 csv to yWriter converter 
+
+Version @release
+Requires Python 3.7 or above
+
+Copyright (c) 2021 Peter Triesberger
+For further information see https://github.com/peter88213/Paeon
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
+import os
+import argparse
+
+from pywriter.ui.ui import Ui
+from pywriter.ui.ui_cmd import UiCmd
+from pywaeon.csv.csv_converter import CsvConverter
+
+SUFFIX = ''
+
+
+def run(sourcePath, silentMode=True):
+
+    if silentMode:
+        ui = Ui('')
+
+    else:
+        ui = UiCmd('csv timeline to yWriter converter @release')
+
+    converter = CsvConverter()
+    converter.ui = ui
+    kwargs = {'suffix': SUFFIX}
+    converter.run(sourcePath, **kwargs)
+    ui.start()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Aeon Timeline 2 csv to yWriter converter',
+        epilog='')
+    parser.add_argument('sourcePath', metavar='Sourcefile',
+                        help='The path of the csv timeline file.')
+
+    parser.add_argument('--silent',
+                        action="store_true",
+                        help='suppress error messages and the request to confirm overwriting')
+    args = parser.parse_args()
+
+    if args.silent:
+        silentMode = True
+
+    else:
+        silentMode = False
+
+    if os.path.isfile(args.sourcePath):
+        sourcePath = args.sourcePath
+
+    else:
+        sourcePath = None
+
+    run(sourcePath, silentMode)
