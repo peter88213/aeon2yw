3d4f1c91781f7769d2e70b28db1ba16acdc93f41
peter88213 <peter.88213@t-online.de>
2021-10-29 20:05:14 +0200

    Add .aeonzip filetype to conversion sources.

--- src/pywaeon2/csv_converter.py
+++ src/pywaeon2/csv_converter.py
@@ -8,11 +8,12 @@ from pywriter.converter.yw_cnv_ui import YwCnvUi
 from pywriter.converter.new_project_factory import NewProjectFactory
 
 from paeon.csv_timeline2 import CsvTimeline2
+from paeon.json_timeline2 import JsonTimeline2
 
 
 class CsvConverter(YwCnvUi):
     """A converter class for csv timeline import."""
-    CREATE_SOURCE_CLASSES = [CsvTimeline2]
+    CREATE_SOURCE_CLASSES = [CsvTimeline2, JsonTimeline2]
 
     def __init__(self):
         YwCnvUi.__init__(self)
